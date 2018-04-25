1a36f528706f85607e914b0551be805334e9aae5
madzia89 <wojcicka.ma@gmail.com>
2018-04-25 14:13:36 +0200

    add multipe elements from an array into a div git push origin master "

--- src/components/MappingArray.js
+++ src/components/MappingArray.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const names = ['Bartek ', 'Magda ', 'Szymon ']
-const MappingArray = (props) => (
+const names = ['Bartek', 'Magda', 'Szymon']
 
-    <div>{names}</div>
+//poniższy div jest tablicą która zawiera elementy JSX dlatego musimy dodać klucze
+const divs = [
+        <div key={1}>{names[0]}</div>,
+        <div key={2}>{names[1]}</div>,
+        <div key={3}>{names[2]}</div>
+]
 
+const MappingArray = () => (
+    <div>
+        {divs}
+    </div>
 )
 
 export default MappingArray
