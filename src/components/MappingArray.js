69833d551ed6bf8361c018771c0f9142afb0dfdb
madzia89 <wojcicka.ma@gmail.com>
2018-04-25 14:18:30 +0200

    easy map

--- src/components/MappingArray.js
+++ src/components/MappingArray.js
@@ -3,15 +3,17 @@ import React from 'react'
 const names = ['Bartek', 'Magda', 'Szymon']
 
 //poniższy div jest tablicą która zawiera elementy JSX dlatego musimy dodać klucze
-const divs = [
-        <div key={1}>{names[0]}</div>,
-        <div key={2}>{names[1]}</div>,
-        <div key={3}>{names[2]}</div>
-]
+// const divs = [
+//         <div key={1}>{names[0]}</div>,
+//         <div key={2}>{names[1]}</div>,
+//         <div key={3}>{names[2]}</div>
+// ]
 
 const MappingArray = () => (
     <div>
-        {divs}
+        {
+            names.map((name,i) => <div key={name}>{name}</div> )
+        }
     </div>
 )
 
