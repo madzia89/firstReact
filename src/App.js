910155c6fc13deff8abce238501779f89d192304
madzia89 <wojcicka.ma@gmail.com>
2018-04-25 14:06:18 +0200

    names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MyfirstComponent from './components/MyFirstComponent'
 import Add from './components/Add'
+import MappingArray from './components/MappingArray'
 import Header from './components/Header'
 
 //poniższe propsy są przekazywane do "funkcji" którą tutaj importujemy
@@ -10,6 +11,8 @@ const App = () => (
         <Add numberA={5} numberB={10}/>
         <Add numberA={565} numberB={121}/>
         <Header text={"A Ala ma kota"} isRed={false}/>
+        <hr/>
+        <MappingArray />
     </div>
 )
 export default App
---
+++ src/components/MappingArray.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const names = ['Bartek ', 'Magda ', 'Szymon ']
+const MappingArray = (props) => (
+
+    <div>{names}</div>
+
+)
+
+export default MappingArray
