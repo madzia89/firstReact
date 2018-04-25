8ba35749c476ada71bd034307990f79bb314806c
madzia89 <wojcicka.ma@gmail.com>
2018-04-25 14:23:15 +0200

    more advanced mapping ;)

--- src/components/MappingArray.js
+++ src/components/MappingArray.js
@@ -1,18 +1,15 @@
 import React from 'react'
 
-const names = ['Bartek', 'Magda', 'Szymon']
-
-//poniższy div jest tablicą która zawiera elementy JSX dlatego musimy dodać klucze
-// const divs = [
-//         <div key={1}>{names[0]}</div>,
-//         <div key={2}>{names[1]}</div>,
-//         <div key={3}>{names[2]}</div>
-// ]
+const users = [
+    {name: 'Bartek', key: "kjajshdd"},
+    {name: 'Magda', key: "kjsdajshdd"},
+    {name: 'Szymon', key: "sadymsadon"}
+    ]
 
 const MappingArray = () => (
     <div>
         {
-            names.map((name,i) => <div key={name}>{name}</div> )
+            users.map((user,i) => <div key={user.key}>{user.name}</div> )
         }
     </div>
 )
