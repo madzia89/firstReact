991c9e7c55074d2208ef18cd617a347ca859bcd0
madzia89 <wojcicka.ma@gmail.com>
2018-04-26 11:50:15 +0200

    putting header in dashboard easy way

--- src/App.js
+++ src/App.js
@@ -8,31 +8,41 @@ import Header from './components/Header'
 import CounterWithState from "./components/CounterWithState"
 import Dashboard from "./components/Dashboard"
 
-//poniższe propsy są przekazywane do "funkcji" którą tutaj importujemy
+//exact
 const App = () => (
     <div>
-
-
         <Router>
             <div>
                 <Link to={'/'}>Home </Link>
                 <Link to={'/mapping-array'}>MappingArray </Link>
                 <Link to={'/mapping-array-two'}>MappingArrayTwo </Link>
                 <Link to={'/counter-with-state'}>CounterWithState </Link>
+                <Link to={'/my-first-component'}>MyfirstComponent </Link>
+                <Link to={'/add-a'}>AddFirst </Link>
+                <Link to={'/add-b'}>AddSecond </Link>
+                <Link to={'/header'}>header </Link>
 
                 <hr/>
                 <Route exact path={'/'} component={Dashboard}/>
                 <Route path={'/mapping-array'} component={MappingArray}/>
                 <Route path={'/mapping-array-two'} component={MappingArrayTwo}/>
                 <Route path={'/counter-with-state'} component={CounterWithState}/>
+                <Route path={'/my-first-component'} render={() => (
+                    <MyfirstComponent name="SomeName"/>
+                )}/>
+                <Route path={'/add-a'} render={() => (
+                    <Add numberA={5} numberB={10}/>
+                )}/>
+                <Route path={'/add-b'} render={() => (
+                    <Add numberA={565} numberB={121}/>
+                )}/>
+                <Route path={'/header'} render={() => (
+                    <Header text={"A Ala ma kota"} isRed={false}/>
+                )}/>
+
 
             </div>
         </Router>
-        <hr/>
-        <MyfirstComponent name="SomeName"/>
-        <Add numberA={5} numberB={10}/>
-        <Add numberA={565} numberB={121}/>
-        <Header text={"A Ala ma kota"} isRed={false}/>
 
 
     </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import Header from './Header'
 const Dashboard = () =>(
+    <div>
     <div>DASHBOARD</div>
+    <Header text={"A Ala ma kota"} isRed={true}/>
+    </div>
 )
 
 export default Dashboard
