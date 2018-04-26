45a04503f91b8a71ef32d2a56597aed542dbea5e
madzia89 <wojcicka.ma@gmail.com>
2018-04-26 11:31:19 +0200

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import MyfirstComponent from './components/MyFirstComponent'
 import Add from './components/Add'
 import MappingArray from './components/MappingArray'
 import MappingArrayTwo from './components/MappingArrayTwo'
 import Header from './components/Header'
 import CounterWithState from "./components/CounterWithState"
+import Dashboard from "./components/Dashboard"
 
 //poniższe propsy są przekazywane do "funkcji" którą tutaj importujemy
 const App = () => (
     <div>
+
+
+        <Router>
+            <div>
+                <Link to={'/'}>Home </Link>
+                <Link to={'/mapping-array'}>MappingArray </Link>
+                <Link to={'/mapping-array-two'}>MappingArrayTwo </Link>
+                <Link to={'/counter-with-state'}>CounterWithState </Link>
+
+                <hr/>
+                <Route exact path={'/'} component={Dashboard}/>
+                <Route path={'/mapping-array'} component={MappingArray}/>
+                <Route path={'/mapping-array-two'} component={MappingArrayTwo}/>
+                <Route path={'/counter-with-state'} component={CounterWithState}/>
+
+            </div>
+        </Router>
+        <hr/>
         <MyfirstComponent name="SomeName"/>
         <Add numberA={5} numberB={10}/>
         <Add numberA={565} numberB={121}/>
         <Header text={"A Ala ma kota"} isRed={false}/>
-        <hr/>
-        <MappingArray/>
-        <hr/>
-        <MappingArrayTwo/>
-        <hr/>
-        <CounterWithState/>
+
+
     </div>
 
 )
---
+++ src/components/Dashboard.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const Dashboard = () =>(
+    <div>DASHBOARD</div>
+)
+
+export default Dashboard
