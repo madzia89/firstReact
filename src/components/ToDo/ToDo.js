2439afdc87976070c86fc29d35164b84494ba71c
madzia89 <wojcicka.ma@gmail.com>
2018-04-27 11:58:46 +0200

    deleteTask

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import List from './List'
 
-class ToDo extends React.Component{
-    render(){
+class ToDo extends React.Component {
+    state = {
+        tasks: [
+            {name: 'umyj naczynia', uid: 82664478218},
+            {name: 'biegaj', uid: 82686478218}
+        ],
+        filterText: ''
+    }
+
+    deleteTask = (taskUid) => {
+        const newTasks = this.state.tasks.filter(task => taskUid !== task.uid)
+        this.setState({
+            tasks: newTasks
+        })
+    }
+
+    render() {
         return (
             <div>
-                ToDo
+                <List
+                    deleteTaskFunction={this.deleteTask}
+                    tasksList={this.state.tasks}
+                />
             </div>
         )
     }
