1f735e18c7e2cec15ee8612ec65fb3bec1983c0c
madzia89 <wojcicka.ma@gmail.com>
2018-04-27 12:06:36 +0200

    deleteTask

--- src/components/ToDo/List.js
+++ src/components/ToDo/List.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Task from './Task'
+//deleteTask powoduje że funkcja nie zostaje odrazu wywołana tylko dopiero gdy deleteTask zostanie kliknięty (bo tak mamy napisane w Task.js gdyby nie było tutuaj () => wówczas deleteTaskFunction od razu próbowałoby się właśnie tutaj wywołać
 
 const List = ({tasksList, deleteTaskFunction}) => (
     <div>
